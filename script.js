947a1b69d2dae994d94f296137dc1dacb2ac0e9b
ROHAN DAS <61402717+rohanbabai2000@users.noreply.github.com>
2025-10-24 20:10:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(myName, myAge){
+		this.name = myName;
+		this.age = myAge;
+	}
+	getName(){
+		return this.name; 
+	}
+	setAge(newAge){
+		this.age = newAge;
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(myName, myAge){
+		super(myName, myAge);
+		
+	}
+	study(){
+			return myName + " is studying";
+		}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(myName, myAge){
+		super(myName, myAge);
+		
+	}
+	teach(){
+		return this.name + " is teaching";
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
